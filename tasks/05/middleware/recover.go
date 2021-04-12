a06cf742c75f509065cd2982daa593e3e17a73d7
Nail Iskhakov <nail@iskhakov.me>
2021-04-12 23:25:12 +0300

    task(5): Middlewares added

--- tasks/05/middleware/recover.go
+++ tasks/05/middleware/recover.go
@@ -6,5 +6,18 @@ import (
 )
 
 func Recover(logger *log.Logger) func(http.Handler) http.Handler {
-	return nil
+	return func(next http.Handler) http.Handler {
+		fn := func(rw http.ResponseWriter, req *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Printf("[ERROR] Panic caught: %v\n", err)
+					rw.WriteHeader(http.StatusInternalServerError)
+					rw.Write([]byte("Internal Server Error\n"))
+				}
+			}()
+			next.ServeHTTP(rw, req)
+		}
+
+		return http.HandlerFunc(fn)
+	}
 }
--- tasks/05/middleware2/limiter.go
+++ tasks/05/middleware2/limiter.go
@@ -2,11 +2,22 @@ package middleware
 
 import (
 	"net/http"
+	"sync"
 )
 
 func Limit(l Limiter) func(http.Handler) http.Handler {
-	// TODO: Implement me
-	return nil
+	return func(next http.Handler) http.Handler {
+		fn := func(rw http.ResponseWriter, r *http.Request) {
+			if l.TryAcquire() {
+				next.ServeHTTP(rw, r)
+				l.Release()
+				return
+			}
+			rw.WriteHeader(http.StatusTooManyRequests)
+		}
+
+		return http.HandlerFunc(fn)
+	}
 }
 
 type Limiter interface {
@@ -15,37 +26,58 @@ type Limiter interface {
 }
 
 type MutexLimiter struct {
-	// TODO: Implement me
+	mutex   *sync.Mutex
+	current int
+	limit   int
 }
 
 func NewMutexLimiter(count int) *MutexLimiter {
-	// TODO: Implement me
-	return nil
+	var m sync.Mutex
+	mutexLimiter := MutexLimiter{
+		mutex:   &m,
+		current: 0,
+		limit:   count,
+	}
+	return &mutexLimiter
 }
 
 func (l *MutexLimiter) TryAcquire() bool {
-	// TODO: Implement me
-	return false
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	if l.current >= l.limit {
+		return false
+	}
+
+	l.current += 1
+
+	return true
 }
 
 func (l *MutexLimiter) Release() {
-	// TODO: Implement me
+	l.mutex.Lock()
+	l.current -= 1
+	l.mutex.Unlock()
 }
 
 type ChanLimiter struct {
-	// TODO: Implement me
+	limiter chan struct{}
 }
 
 func NewChanLimiter(count int) *ChanLimiter {
-	// TODO: Implement me
-	return nil
+	chanLimiter := ChanLimiter{limiter: make(chan struct{}, count)}
+	return &chanLimiter
 }
 
 func (l *ChanLimiter) TryAcquire() bool {
-	// TODO: Implement me
-	return false
+	select {
+	case l.limiter <- struct{}{}:
+		return true
+	default:
+		return false
+	}
 }
 
 func (l *ChanLimiter) Release() {
-	// TODO: Implement me
+	_ = <-l.limiter
 }
